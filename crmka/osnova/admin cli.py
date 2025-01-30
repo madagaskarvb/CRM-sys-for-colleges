f8286b4e6d27aa4231b22ec3045edc8f5e2ba7d2
madagaskarvb <149654664+madagaskarvb@users.noreply.github.com>
2025-01-30 09:27:50 +0300

    Update admin cli.py
    added checks to the existance of tables and added changes_in_table()

--- crmka/osnova/admin cli.py
+++ crmka/osnova/admin cli.py
@@ -58,19 +58,50 @@ def cleanup_deleted_users():
 
 def changes_in_table():
     name = input("Table name: ")
-    cursor.execute(f"UPDATE {name} SET attribute = {value} WHERE attribute2 = {value2}")
+    cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{name}'")
+    table_exists = cursor.fetchone()
+    if not table_exists:
+        # If the table does not exist, print a message
+        print(f"Table '{name}' does not exist.")
+        return
+    
+    set = input("SET: ")
+    where = input("WHERE: ")
+    try:
+        cursor.execute(f"UPDATE {name} SET {set} WHERE {where}")
+        connection.commit()
+        print("Data added!")
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
 
 def write_in_table():
     name = input("Table name: ")
+    cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{name}'")
+    table_exists = cursor.fetchone()
+    if not table_exists:
+        # If the table does not exist, print a message
+        print(f"Table '{name}' does not exist.")
+        return
+
     data = input("Input data: ")
     try:
-        cursor.execute("""INSERT INTO {name} VALUES {data}""")
+        cursor.execute(f"""INSERT INTO {name} VALUES {data}""")
         connection.commit()
         print("Data added!")
     except sqlite3.Error as e:
         print(f"An error occurred: {e}")
 
 def create_table():
+    table_name = input("Enter the name of the table: ").strip()
+
+    cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'")
+    table_exists = cursor.fetchone()
+
+    if not table_exists:
+        # If the table does not exist, print a message
+        print(f"Table '{table_name}' does not exist.")
+        return
+
     table_name = input("Enter the name of the table: ").strip()
     table_attributes = input(f"Enter the attributes for the table '{table_name}': ").strip()
     create_table_query = f"CREATE TABLE {table_name} ({table_attributes});"
@@ -86,6 +117,7 @@ def delete_table():
     table_delete = input("Name of table to delete: ")
     cursor.execute(f"DROP TABLE IF EXISTS {table_delete}")
     connection.commit()
+    print("Table deleted!")
 
 
 # Запись удалённого (точнее, помеченного на удаление) пользователя в deleted_users.json
