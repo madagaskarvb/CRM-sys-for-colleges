5804f2f004b9dfb51f978bf430d7aa99e287e1db
Катя <katya.kotel@bk.ru>
2025-02-04 22:31:23 +0300

    hdbhb

--- crmka/locallibrary/catalog/urls.py
+++ crmka/locallibrary/catalog/urls.py
@@ -1,8 +1,9 @@
-
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
-
+    path('student/', views.student_page, name='student_page'),
+    path('teacher/', views.teacher_page, name='teacher_page'),
+    path('admin/', views.admin_page, name='admin_page'),
 ]
--- crmka/locallibrary/catalog/views.py
+++ crmka/locallibrary/catalog/views.py
@@ -3,17 +3,26 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Student
 
-def index(request):
-    """
-    Функция отображения для домашней страницы сайта.
-    """
-    # Генерация "количеств" некоторых главных объектов
-    num_users = Student.objects.all().count()
+# def index(request):
+#     """
+#     Функция отображения для домашней страницы сайта.
+#     """
+#     # Генерация "количеств" некоторых главных объектов
+#     num_users = Student.objects.all().count()
 
-    # Отрисовка HTML-шаблона index.html с данными внутри
-    # переменной контекста context
-    return render(
-        request,
-        'index.html',
-        context={'num_users':num_users},
-    )
+#     # Отрисовка HTML-шаблона index.html с данными внутри
+#     # переменной контекста context
+#     return render(
+#         request,
+#         'index.html',
+#         context={'num_users':num_users},
+#     )
+
+def student_page(request):
+    return render(request, 'basikPages/studentPage.html')
+
+def teacher_page(request):
+    return render(request, 'basikPages/teacherPage.html')
+
+def admin_page(request):
+    return render(request, 'basikPages/adminPage.html')
