e4bc7c811e76878c0eb604f373c4bea81678a3ed
madagaskarvb <149654664+madagaskarvb@users.noreply.github.com>
2025-02-10 10:11:15 +0300

    Update admin.py

--- crmka/locallibrary/catalog/admin.py
+++ crmka/locallibrary/catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student, Teacher, Grade, Faculty, EducationalMaterial
+from .models import Students, Teachers, Grades, Faculty, EducationalMaterials, GroupSubject, Subjects, Groups
 from .forms import UserAdminForm
 
 class UserAdmin(admin.ModelAdmin):
@@ -17,8 +17,11 @@ class UserAdmin(admin.ModelAdmin):
     )
 
 # Регистрируем каждую модель отдельно
-admin.site.register(Student, UserAdmin)
-admin.site.register(Teacher)
-admin.site.register(Grade)
+admin.site.register(Students, UserAdmin)
+admin.site.register(Teachers)
+admin.site.register(Grades)
 admin.site.register(Faculty)
-admin.site.register(EducationalMaterial)
+admin.site.register(EducationalMaterials)
+admin.site.register(GroupSubject)
+admin.site.register(Subjects)
+admin.site.register(Groups)
